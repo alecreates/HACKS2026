a0ac1263f88b9b2c61dd511ff91b1fb24d001903
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-02-06 22:02:22 -0500

    register component frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Profile from "./profile/profile";
 import Login from "./login/login";
+import Register from "./register/register";
 
 export default function Home() {
   return (
-   <Login></Login>
+   <Register></Register>
   );
 }
--- app/register/register.tsx
+++ app/register/register.tsx
@@ -1,13 +1,63 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./register.module.css";
 
-const Register = ({}) => {
+const Register = ({ }) => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [neighborhoodId, setNeighborhoodId] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('Register form submitted:', { username, password });
+  };
 
   return (
-    <div>
-      Register Page
+    <div className={styles.container}>
+
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <div className={styles.header}>Register</div>
+        <div className={styles.field}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="password">Neighborhood ID</label>
+          <input
+            type="password"
+            id="neighborhoodId"
+            value={neighborhoodId}
+            onChange={(e) => setNeighborhoodId(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.button}>Register</button>
+        <div className={styles.header}>Already have an account?</div>
+        <div className={styles.header}>Log In</div>
+        
+      </form>
+
+
     </div>
   );
 };
