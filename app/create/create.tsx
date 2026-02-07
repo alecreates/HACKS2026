7953fd7742b7c8c47ee118fbfcc4f2db9f2080bc
keira-inks <keira.tintas23@gmail.com>
2026-02-07 10:11:00 -0500

    Card styling

--- app/create/create.tsx
+++ app/create/create.tsx
@@ -4,23 +4,26 @@ import React from "react";
 import styles from "./create.module.css";
 import Image from "next/image";
 import myImage from "./images/jake.jpg";
-const Create = ({}) => {
+const Create = ({ }) => {
 
   return (
-    <div className={styles.card}>
-      <Image 
-      src={myImage}
-      alt="Avatar" 
-      width={200}
-      height={200}
-      />
-      <div className={styles.container}>
-        <h4><b>Jake F. State-Farm</b></h4>
-        <p>Company Representative</p>
-  </div>
-</div>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <Image
+          src={myImage}
+          alt="Avatar"
+          width={200}
+          height={200}
+        />
+        <div className={styles.infoContainer}>
+          <h4><b>Jake F. State-Farm</b></h4>
+          <p>Company Representative</p>
+        </div>
+      </div>
+    </div>
 
-    
+
+
   );
 };
 
