b7b4e5460fff79181f8d1bede71afeba31c7db63
keira-inks <keira.tintas23@gmail.com>
2026-02-07 11:39:57 -0500

    Create post card

--- app/create/create.tsx
+++ app/create/create.tsx
@@ -1,28 +1,65 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import styles from "./create.module.css";
 import Image from "next/image";
 import myImage from "./images/jake.jpg";
-const Create = ({ }) => {
+
+
+
+const Create = () => {
+  
+  const [request, setRequest] = useState('');
+  const [offer, setOffer] = useState('');
+
+  const handlePost = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('Post Created:', { request, offer });
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-        <Image
-          src={myImage}
-          alt="Avatar"
-          width={200}
-          height={200}
-        />
-        <div className={styles.infoContainer}>
-          <h4><b>Jake F. State-Farm</b></h4>
-          <p>Company Representative</p>
+      
+      <form onSubmit={handlePost} className={styles.createForm}>
+        <div className={styles.card}>
+
+          {/* Header */}
+          <div className={styles.header}>
+            <span className={styles.name}>Create Post!</span>
+          </div>
+          
+          {/* Title */}
+          <h2 className={styles.title}>
+            Your request:
+          </h2>
+          <div className={styles.field}>
+            <input
+              type="text"
+              id="request"
+              value={request}
+              onChange={(e) => setRequest(e.target.value)}
+            />
+          </div>
+
+
+          {/* Offer */}
+          <h2 className={styles.title}>
+            Your Offer:
+          </h2>
+<div className={styles.field}>
+            <input
+              type="text"
+              id="offer"
+              value={offer}
+              onChange={(e) => setOffer(e.target.value)}
+            />
+          </div>
+          <button type="submit" className={styles.postButton}>
+            Post
+          </button>
         </div>
-      </div>
+      </form>
     </div>
-
-
 
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,10 @@ import Image from "next/image";
 import Profile from "./profile/profile";
 import Login from "./login/login";
 import Create from "./create/create";
+import Listing from "./listing/listing";
 
 export default function Home() {
   return (
-    <Listing
-    displayName="Alex Neighbor"
-    request="Need help fixing my fence"
-    offer=
-      "Home-cooked meal"
-    createdAt="5 days ago" 
-  />
+    <Create></Create>
   );
 }
