bf92b76cf45ba04e6013e1a0af6313777ce540cd
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-02-07 10:14:52 -0500

    Created listings component

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import Profile from "./profile/profile";
 import Login from "./login/login";
 import Register from "./register/register";
+import Listing from "./listing/listing";
 
 export default function Home() {
   return (
-   <Register></Register>
+   <Listing></Listing>
   );
 }
