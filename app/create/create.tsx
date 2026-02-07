2469c0f653f4c6867818cf181fcfa4a69dc28eb1
keira-inks <keira.tintas23@gmail.com>
2026-02-06 22:13:23 -0500

    Began card component

--- app/create/create.tsx
+++ app/create/create.tsx
@@ -2,13 +2,23 @@
 
 import React from "react";
 import styles from "./create.module.css";
-
 const Create = ({}) => {
 
   return (
-    <div>
-      Create Post here!!
-    </div>
+    <div className={styles.card}>
+      <img 
+      src="./images/jake.jpg" 
+      alt="Avatar" 
+      width={200}
+      height={200}
+      />
+      <div className={styles.container}>
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+  </div>
+</div>
+
+    
   );
 };
 
