222f20d0abdfd75066f14a51f7dd840712369996
Alexia Toma <alexia.g.toma.328@gmail.com>
2026-02-07 10:44:56 -0500

    began feed page front end

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import Profile from "./profile/profile";
 import Login from "./login/login";
 import Register from "./register/register";
 import Listing from "./listing/listing";
+import Feed from "./feed/feed";
 
 export default function Home() {
   return (
-    <Listing
-    displayName="Alex Neighbor"
-    request="Need help fixing my fence"
-    offer=
-      "Home-cooked meal"
-    createdAt="5 days ago" 
-  />
+    
+    <Feed></Feed>
+
+  //   <Listing
+  //   displayName="Alex Neighbor"
+  //   request="Need help fixing my fence"
+  //   offer=
+  //     "Home-cooked meal"
+  //   createdAt="5 days ago" 
+  // />
   );
 }
