f57cd3a18200095458c4786a077212b56cc67863
keira-inks <keira.tintas23@gmail.com>
2026-02-07 11:49:26 -0500

    create component front end

--- app/create/create.tsx
+++ app/create/create.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styles from "./create.module.css";
-import Image from "next/image";
-import myImage from "./images/jake.jpg";
-
-
 
 const Create = () => {
-  
+
   const [request, setRequest] = useState('');
   const [offer, setOffer] = useState('');
 
@@ -19,18 +15,18 @@ const Create = () => {
 
   return (
     <div className={styles.container}>
-      
+
       <form onSubmit={handlePost} className={styles.createForm}>
         <div className={styles.card}>
 
           {/* Header */}
           <div className={styles.header}>
-            <span className={styles.name}>Create Post!</span>
+            <span className={styles.name}>Create Post</span>
           </div>
-          
+
           {/* Title */}
           <h2 className={styles.title}>
-            Your request:
+            Your Request:
           </h2>
           <div className={styles.field}>
             <input
@@ -46,7 +42,7 @@ const Create = () => {
           <h2 className={styles.title}>
             Your Offer:
           </h2>
-<div className={styles.field}>
+          <div className={styles.field}>
             <input
               type="text"
               id="offer"
